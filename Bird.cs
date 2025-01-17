c1fdf20d75f5c8e97207db38f10262ab8dcccdfd
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-01-17 16:22:09 +0100

    add lesson1 progress

---
+++ Bird.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    public class Bird
+    {
+        private Vector2 position;
+        private Vector2 velocity;
+        private float gravity;
+        private const float flapStrength = -8.0f;
+        private const int size = 20;
+        private const float maxVelocity = 10.0f;
+
+        public Vector2 Position => position;
+
+        public Bird(Vector2 startPosition, float gravity)
+        {
+            this.position = startPosition;
+            this.gravity = gravity;
+            this.velocity = new Vector2(0, 0);
+        }
+
+        public void Update()
+        {
+            velocity.Y += gravity;
+            position += velocity;
+
+            if (velocity.Y > maxVelocity)
+            {
+                velocity.Y = maxVelocity;
+            }
+
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+                velocity.Y = 0;
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,87 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlappyBird
+{
+    public class Game
+    {
+        private GameStatus gameStatus;
+        private const int screenWidth = 800;
+        private const int screenHeight = 600;
+        private float gravity = 0.5f;
+        private int countdown = 3;
+        private float countdownTimer = 0f;
+        private int score = 0;
+
+        public void Initialize()
+        {
+            Raylib.InitWindow(screenWidth, screenHeight, "Flappy pigeon");
+            Raylib.SetTargetFPS(144);
+
+            gameStatus = GameStatus.Ready;
+            countdown = 3;
+            countdownTimer = 0f;
+            score = 0;
+        }
+
+        public void Update()
+        {
+            if (gameStatus == GameStatus.Ready)
+            {
+                countdownTimer += Raylib.GetFrameTime();
+
+                if (countdownTimer >= 1)
+                {
+                    countdown--;
+                    countdownTimer = 0;
+                }
+
+                if (countdown <= 0)
+                {
+                    gameStatus = GameStatus.Playing;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib_cs.Color.SkyBlue);
+
+            if (gameStatus == GameStatus.Ready)
+            {
+                Raylib.DrawText(countdown.ToString(), screenWidth / 2 - 20, screenHeight / 2 - 20, 60, Raylib_cs.Color.Black);
+            }
+            else
+            {
+                if (gameStatus == GameStatus.GameOver)
+                {
+                    Raylib.DrawText("Game Over", screenWidth / 2 - 160, screenHeight / 2 - 50, 70, Raylib_cs.Color.Red);
+
+                    Raylib.DrawText("Press Enter to Restart", screenWidth / 2 - 170, screenHeight / 2 + 30, 30, Raylib_cs.Color.Red);
+                }
+
+                Raylib.DrawText("Score: " + score, 10, 10, 30, Raylib_cs.Color.Black);
+            }
+
+            Raylib.EndDrawing();
+        }
+
+        public void Run()
+        {
+            Initialize();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Draw();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
